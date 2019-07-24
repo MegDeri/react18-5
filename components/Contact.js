f5aa2c7cfadd2d198aa405ced8f2e195848a1271
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-07-25 01:19:26 +0300

    Change contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -11,6 +11,7 @@ var Contact = React.createClass({
             src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
           }),
           React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          console.log(this.props.item.firstName),
           React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
           React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
             this.props.item.email
